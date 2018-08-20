0278b62b5d7fedc1df838a8b692463b6c98a8e21
Joseph Utecht <joseph.utecht@gmail.com>
2018-08-20 13:44:50 -0500

    Initial Commit

---
+++ metal_tut/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  metal_tut
+//
+//  Created by Joseph Utecht on 8/20/18.
+//  Copyright © 2018 Joseph Utecht. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ metal_tut/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  metal_tut
+//
+//  Created by Joseph Utecht on 8/20/18.
+//  Copyright © 2018 Joseph Utecht. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
